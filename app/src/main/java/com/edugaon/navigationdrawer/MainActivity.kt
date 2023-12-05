e94408ef46c6c0b104a82156810c3b32e0c06412
ansar <ansarali.edugaon@gmail.com>
2023-12-05 22:08:29 +0530

    first commit
    adding project to the repo

---
+++ app/src/main/java/com/edugaon/navigationdrawer/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.edugaon.navigationdrawer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.drawerlayout.widget.DrawerLayout
+
+class MainActivity : AppCompatActivity() {
+    lateinit var drawerLayout: DrawerLayout
+    lateinit var  actionBarDrawerToggle: ActionBarDrawerToggle
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        // init drawer layout
+        drawerLayout = findViewById(R.id.myDrawer_layout)
+        // init action bar drawer toggle
+        actionBarDrawerToggle = ActionBarDrawerToggle(this,drawerLayout,R.string.nav_open,R.string.nav_close)
+        // add a drawer listener into  drawer layout
+        drawerLayout.addDrawerListener(actionBarDrawerToggle)
+        actionBarDrawerToggle.syncState()
+
+        // show home icon on the app bar
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return if (actionBarDrawerToggle.onOptionsItemSelected(item)){
+            true
+        }else{
+            super.onOptionsItemSelected(item)
+        }
+
+    }
+}
